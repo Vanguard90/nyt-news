90544126449fd8085e66a05156ebc2adbe8cc7d2
Vanguard90 <enesmil@gmail.com>
2017-08-29 05:21:23 +0200

    Added react framework. First component added.

---
+++ nyt-news/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from 'react-dom';
+import './css/style.css';
+
+
+import App from './components/App';
+
+render(<App/>, document.querySelector("#root"));
