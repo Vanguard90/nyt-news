09128f1b461b6d6e06424dc6c4a8a48c06e3458a
Vanguard90 <enesmil@gmail.com>
2017-09-12 20:10:57 +0200

    Made an attempt on loading data to state, not very successful

--- nyt-news/src/components/App.js
+++ nyt-news/src/components/App.js
@@ -4,6 +4,36 @@ import Masthead from "./Masthead";
 import NewsBox from "./NewsBox";
 
 class App extends React.Component {
+	constructor() {
+		super();
+		this.addNews = this.addNews.bind(this);
+		this.state = {
+
+			news: {}
+			
+		};
+	}
+
+	addNews(newsdata) {
+
+		const news = {...this.state.news};
+		const timestamp = Date.now();
+		news[`newsdata-${timestamp}`] = news;
+		this.setState({ news });
+	}
+
+	componentDidMount() {
+
+		let news = fetch('https://api.nytimes.com/svc/topstories/v2/home.json?api-key=3b6e4cf6acee482fb77482cfd4416c0b');
+
+		news
+		.then(data => data.json())
+		.then(data => {console.log(data)})
+		.catch((err) => { console.log(err)});
+
+		news = news.results;
+	}
+
 	render () {
 		return (
 			<div>
