fedc6a789ed68c8fbd6a2cae6dca67bc7ed32d0d
Vanguard90 <enesmil@gmail.com>
2017-10-08 22:39:24 +0200

    Header component is now a functional component

--- nyt-news/src/components/Header.js
+++ nyt-news/src/components/Header.js
@@ -1,15 +1,14 @@
 import React from 'react';
 
-class Header extends React.Component {
-	render () {
-		return (
-			<header className="header-holder">
-			<nav className="header-item">Go to NYTimes</nav>
-			<nav className="header-item"> Today's paper</nav>
-			<p className="header-item">Date & Time </p>
-			</header>
-		)
-	}
+//Below is a functional component
+function Header(){
+	return (
+		<header className="header-holder">
+		<nav className="header-item">Go to NYTimes</nav>
+		<nav className="header-item"> Today's paper</nav>
+		<p className="header-item">Date & Time </p>
+		</header>
+	)
 }
 
 export default Header;
