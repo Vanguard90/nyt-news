62c3c27679f56cb9e49b3df9c1f9b71619a2a255
Vanguard90 <IST455@DESKTOP-00L6OHL.local.istatease.com>
2017-10-13 21:35:00 +0200

    Meaningless edit to show up on the counter

--- nyt-news/src/components/Footer.js
+++ nyt-news/src/components/Footer.js
@@ -4,7 +4,7 @@ let footer = function {
 	return (
 		<footer>
 		<div className="footer">
-		<p> Made with love by Enes Kirimi</p>
+		<p> Made with love by Enes Kirimi!</p>
 		</div>
 		</footer>
 	)
