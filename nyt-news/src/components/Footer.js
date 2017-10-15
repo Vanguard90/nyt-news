54e39523798fcc851c2f39bc731a8a9a003a88ea
Vanguard90 <enesmil@gmail.com>
2017-10-15 20:21:31 +0200

    Lower renditions configured. App ready to deploy.

--- nyt-news/src/components/Footer.js
+++ nyt-news/src/components/Footer.js
@@ -5,8 +5,8 @@ let Footer = function() {
 	return (
 		<footer>
 		<div className="footer">
-		<p className="owner"> Made with love by <span>Enes Kirimi</span></p>
 		<div className="disclaimer"><img src={logoAPI} alt="nyt-logo-diclaimer-api"/></div>
+		<p className="owner"> Made with love by <span>Enes Kirimi</span></p>
 		</div>
 		</footer>
 	)
