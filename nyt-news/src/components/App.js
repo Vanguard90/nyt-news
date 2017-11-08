19fe17441bf99c0e91e14d414be762b6a88dbcc9
Vanguard90 <enesmil@gmail.com>
2017-11-08 20:45:11 +0100

    Some progress on adding filters. Roadmap defined. Have sections in an Array.

--- nyt-news/src/components/App.js
+++ nyt-news/src/components/App.js
@@ -9,7 +9,8 @@ class App extends React.Component {
 	constructor() {
 		super();
 		this.state = {
-			news: {}
+			news: {},
+			sections: {}
 		};
 	}
 
@@ -19,6 +20,21 @@ class App extends React.Component {
 
 	}
 
+
+sectionFilter() {
+
+let holder = Object.keys(this.state.news).map(key => this.state.news[key].section); //This gives me an array of all sections.
+
+//Reduce the holder to find the number of spesific sections.
+//Update state with spesific section data so that app knows what type of sections we have and how many. Naturally reduce to an object.
+//Use this data to populate a dynamic filter section with input tags and checkboxes. Seperate component. 
+//Listen to these checkboxes. Call function onclick. 
+//Look for these sections at the Newsbox components. Possible use of reference.
+//Display:none on ones that are not selected.
+//Refresh steps 3/4/5 to update.
+
+}
+
 	apiCall() {
 
 	let news = fetch('https://api.nytimes.com/svc/topstories/v2/home.json?api-key=3b6e4cf6acee482fb77482cfd4416c0b');
@@ -27,6 +43,8 @@ class App extends React.Component {
 	.then(data => data.json())
 	.then(data => {this.setState({ news: data.results })})
 	.catch((err) => { console.log(err)});
+
+	this.sectionFilter();
 	}
 
 	render() {
