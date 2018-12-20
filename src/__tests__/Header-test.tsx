cac5b70e19af8b2e4c3f59fd5b2f7bf31a152cf6
Enes Kirimi <enesmil@gmail.com>
2018-12-20 16:23:36 +0100

    Increased header tests to cover all switches.

--- src/__tests__/Header-test.tsx
+++ src/__tests__/Header-test.tsx
@@ -22,8 +22,74 @@ describe('Header', () => {
         expect(shallow(<Header />).find('svg').length).toBe(1);
     })
 
-    it('displays correct date', () => {
-        const headerComponent = mount(<Header />);
+    it('displays correct date for January & Monday', () => {
+        const headerComponent = shallow(<Header />);
+        headerComponent.setState( { day: 1, month: "January", year: 2018, weekDay: "Monday"} );
+       expect(headerComponent.find('p').html()).toContain('Monday, 1 January, 2018');
+    })
+
+    it('displays correct date for February & Wednesday', () => {
+        const headerComponent = shallow(<Header />);
+        headerComponent.setState( { day: 7, month: "February", year: 2018, weekDay: "Wednesday"} );
+       expect(headerComponent.find('p').html()).toContain('Wednesday, 7 February, 2018');
+    })
+
+    it('displays correct date for March & Thursday', () => {
+        const headerComponent = shallow(<Header />);
+        headerComponent.setState( { day: 1, month: "March", year: 2018, weekDay: "Thursday"} );
+       expect(headerComponent.find('p').html()).toContain('Thursday, 1 March, 2018');
+    })
+
+    it('displays correct date for April & Friday', () => {
+        const headerComponent = shallow(<Header />);
+        headerComponent.setState( { day: 6, month: "April", year: 2018, weekDay: "Friday"} );
+       expect(headerComponent.find('p').html()).toContain('Friday, 6 April, 2018');
+    })
+
+    it('displays correct date for May & Saturday', () => {
+        const headerComponent = shallow(<Header />);
+        headerComponent.setState( { day: 5, month: "May", year: 2018, weekDay: "Saturday"} );
+       expect(headerComponent.find('p').html()).toContain('Saturday, 5 May, 2018');
+    })
+
+    it('displays correct date for June & Sunday', () => {
+        const headerComponent = shallow(<Header />);
+        headerComponent.setState( { day: 3, month: "June", year: 2018, weekDay: "Sunday"} );
+       expect(headerComponent.find('p').html()).toContain('Sunday, 3 June, 2018');
+    })
+
+    it('displays correct date for July', () => {
+        const headerComponent = shallow(<Header />);
+        headerComponent.setState( { day: 1, month: "July", year: 2018, weekDay: "Sunday"} );
+       expect(headerComponent.find('p').html()).toContain('Sunday, 1 July, 2018');
+    })
+
+    it('displays correct date for August', () => {
+        const headerComponent = shallow(<Header />);
+        headerComponent.setState( { day: 5, month: "August", year: 2018, weekDay: "Sunday"} );
+       expect(headerComponent.find('p').html()).toContain('Sunday, 5 August, 2018');
+    })
+
+    it('displays correct date for September', () => {
+        const headerComponent = shallow(<Header />);
+        headerComponent.setState( { day: 2, month: "September", year: 2018, weekDay: "Sunday"} );
+       expect(headerComponent.find('p').html()).toContain('Sunday, 2 September, 2018');
+    })
+
+    it('displays correct date for October', () => {
+        const headerComponent = shallow(<Header />);
+        headerComponent.setState( { day: 7, month: "October", year: 2018, weekDay: "Sunday"} );
+       expect(headerComponent.find('p').html()).toContain('Sunday, 7 October, 2018');
+    })
+
+    it('displays correct date for November', () => {
+        const headerComponent = shallow(<Header />);
+        headerComponent.setState( { day: 4, month: "November", year: 2018, weekDay: "Sunday"} );
+       expect(headerComponent.find('p').html()).toContain('Sunday, 4 November, 2018');
+    })
+
+    it('displays correct date for December & Tuesday', () => {
+        const headerComponent = shallow(<Header />);
         headerComponent.setState( { day: 11, month: "December", year: 2018, weekDay: "Tuesday"} );
        expect(headerComponent.find('p').html()).toContain('Tuesday, 11 December, 2018');
     }) // // Do this for all other posibilities to increase the coverage.
