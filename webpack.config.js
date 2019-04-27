d03d2f22efd8c14165f771b302d389768287874e
Vanguard90 <enesmil@gmail.com>
2019-04-27 18:35:54 +0200

    Improved the build process setup. Rermoved duplicate remnant index.html

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 const HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
   template: path.join(__dirname, '/src/index.html'),
@@ -7,49 +9,57 @@ const HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
   inject: 'body',
 });
 
-module.exports = {
-  devServer: {
-    host: 'localhost',
-    port: '3000',
-    headers: {
-      'Access-Control-Allow-Origin': '*',
+const copyWebpackPluginConfig = new CopyPlugin([{
+  from: './public',
+  to: './'
+}, ]);
+
+module.exports = (env, options) => {
+  console.log(`--- Webpack is running in ${options.mode} mode ---`);
+  const selectedModeIsProd = options.mode === 'production' ? true : false;
+  return {
+    devServer: {
+      host: 'localhost',
+      port: '3000',
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
+      historyApiFallback: true,
     },
-    historyApiFallback: true,
-  },
-  entry: [path.join(__dirname, '/src/index.tsx')],
-  output: {
-    filename: '[name].bundle.js',
-    chunkFilename: '[name].bundle.js',
-    path: path.join(__dirname, '/build'),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(t|j)sx?$/,
-        exclude: /node_modules/,
-        loaders: ['awesome-typescript-loader'],
-      },
-      {
-        test: /\.scss$|\.css$/,
-        loader: 'style-loader!css-loader!sass-loader',
-      },
-      {
-        test: /\.(jpe?g|png|gif|svg)$/i,
-        loader: 'url-loader',
-        options: {
-          limit: 10000,
+    entry: [path.join(__dirname, '/src/index.tsx')],
+    output: {
+      filename: '[name].bundle.js',
+      chunkFilename: '[name].bundle.js',
+      path: path.join(__dirname, '/build'),
+    },
+    module: {
+      rules: [{
+          test: /\.(t|j)sx?$/,
+          exclude: /node_modules/,
+          loaders: ['awesome-typescript-loader'],
         },
+        {
+          test: /\.scss$|\.css$/,
+          loader: 'style-loader!css-loader!sass-loader',
+        },
+        {
+          test: /\.(jpe?g|png|gif|svg)$/i,
+          loader: 'url-loader',
+          options: {
+            limit: 10000,
+          },
+        },
+      ],
+    },
+    devtool: 'source-map',
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js', '.jsx'],
+    },
+    plugins: selectedModeIsProd ? [new CleanWebpackPlugin(), HTMLWebpackPluginConfig, copyWebpackPluginConfig] : [HTMLWebpackPluginConfig],
+    optimization: {
+      splitChunks: {
+        chunks: 'all',
       },
-    ],
-  },
-  devtool: 'source-map',
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.jsx'],
-  },
-  plugins: [HTMLWebpackPluginConfig],
-  optimization: {
-     splitChunks: {
-       chunks: 'all',
-     },
-   },
+    },
+  };
 };
