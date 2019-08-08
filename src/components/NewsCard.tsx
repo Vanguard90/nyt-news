15b252d4dd20c8efaf42d20a9fdc8a3a15e2bfcd
Vanguard90 <enesmil@gmail.com>
2019-08-09 00:44:04 +0200

    NewsCard component is now a functional component

--- src/components/NewsCard.tsx
+++ src/components/NewsCard.tsx
@@ -9,26 +9,24 @@ interface INewsCardProps {
 	key: number;
 }
 
-class NewsCard extends React.Component<INewsCardProps> {
-	render() {
+const NewsCard = (props: INewsCardProps) => {
 		return (
 			<div className="menu-news">
 				<div className="NewsCard">
 					<div className="photo-thumbnail">
-						<img className="thumbnail-image" src={this.props.multimediaurl} alt="news-thumbnail" />
+						<img className="thumbnail-image" src={props.multimediaurl} alt="news-thumbnail" />
 					</div>
 					<div className="text-section">
-						<h3>{this.props.title}</h3>
-						<p>{this.props.abstract}</p>
+						<h3>{props.title}</h3>
+						<p>{props.abstract}</p>
 					</div>
 				</div>
 				<div className="link-holder">
-					<p className="read-at"><a href={this.props.readurl}><span>Read at NYTimes >></span></a></p>
+					<p className="read-at"><a href={props.readurl}><span>Read at NYTimes >></span></a></p>
 				</div>
-				<div className="tag"><p><b>Section:</b> {this.props.section}</p></div>
+				<div className="tag"><p><b>Section:</b> {props.section}</p></div>
 			</div>
 		)
-	}
 }
 
 export default NewsCard;
