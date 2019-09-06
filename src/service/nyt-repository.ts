c3eeb069df5bbef1595982c9bf34e6a9addd76dd
Vanguard90 <enesmil@gmail.com>
2019-09-06 19:11:04 +0200

    Fixed formatting

--- src/service/nyt-repository.ts
+++ src/service/nyt-repository.ts
@@ -1,6 +1,6 @@
 import { ITopStories } from './service-interface';
 
-const apikey = '3b6e4cf6acee482fb77482cfd4416c0b'; 
+const apikey = '3b6e4cf6acee482fb77482cfd4416c0b';
 // For the people looking at this repository: This is public key you can get in 10m from NYT,
 // So no need to hide it in environmental variables etc. It's not necessarily a private key.
 
@@ -13,7 +13,7 @@ export async function getTopStories(): Promise<ITopStories> {
                 result = response.json();
             }
            }).catch((err) => {
-            console.error('gettopStories fetch error: ', err);
+            console.error('getTopStories fetch error: ', err);
            });
         return result; // Note that perhaps try/catch is a better option here!!!
 }
