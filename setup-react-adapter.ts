c834a721b4c9a559566050c32f99013696f2e4bc
Vanguard90 <enesmil@gmail.com>
2020-02-09 13:10:44 +0100

    Fetch can now be mocked.

--- setup-react-adapter.ts
+++ setup-react-adapter.ts
@@ -1,5 +1,6 @@
 import enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+require('jest-fetch-mock').enableMocks();
 enzyme.configure({ adapter: new Adapter() });
 
 // This file is for creating an adapter before every test, so that we are not repeating it at every test file
